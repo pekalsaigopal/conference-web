2521c5ee8e19b914c9c3a77063ee9155a968ab95
saigopal <saigopalpekal@gmail.com>
2020-11-18 17:45:27 +0530

    validating objects in spring

--- src/main/java/in/psg/conferenceweb/model/Registration.java
+++ src/main/java/in/psg/conferenceweb/model/Registration.java
@@ -1,6 +1,10 @@
 package in.psg.conferenceweb.model;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Registration {
+
+    @NotEmpty
     private String name;
 
     public String getName() {
